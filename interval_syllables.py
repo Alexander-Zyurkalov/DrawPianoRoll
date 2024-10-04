a14e910e9a0f930fecbf77c73f2aa582c409502b
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-10-04 18:37:19 +0200

    Add showing traditional name of the interval

--- interval_syllables.py
+++ interval_syllables.py
@@ -7,9 +7,17 @@ mi_do = 'e'
 ma_do = 'o'
 pe_do = 'y'
 tr_up = ''
-
-qualities = [root_suffix, mi_up, ma_up, mi_up, ma_up, pe_up, tr_up, pe_up, mi_up, ma_up, mi_up, ma_up, pe_up+pe_up]
-assert len(bases) == len(qualities)
+tr_do = ''
+intervalQualities = {
+   mi_up: 'm',
+   ma_up: 'M',
+   pe_up: 'P',
+   tr_up: ''
+}
+intervalNums = ['P1', 'm2', 'M2', 'm3', 'M3', 'P4', 'Triton', 'P5', 'm6', 'M6', 'm7', 'M7', 'Octave']
+up_qualities = [root_suffix, mi_up, ma_up, mi_up, ma_up, pe_up, tr_up, pe_up, mi_up, ma_up, mi_up, ma_up, pe_up + pe_up]
+do_qualities = [root_suffix, mi_do, ma_do, mi_do, ma_do, pe_do, tr_do, pe_do, mi_do, ma_do, mi_do, ma_do, pe_do + pe_do]
+assert len(bases) == len(up_qualities)
 
 count = 0
 for i, root in enumerate(bases):
@@ -17,11 +25,13 @@ for i, root in enumerate(bases):
       break
    for j in range(12):
       interval_index = (i + j) % 12
-      q = qualities[j]
+      up_quality = up_qualities[j]
+      do_quality = do_qualities[j]
       n_note = bases[interval_index]
       if root == n_note:
          continue
       count += 1
-      variant = root + root_suffix + n_note + q
-      print(variant)
+      leftToRight = root + root_suffix + n_note + up_quality
+      rightToLeft = n_note + root_suffix + root + do_quality
+      print(f"{leftToRight}\tmnemonic\t{rightToLeft}\t{intervalNums[j]}")
 print("count = ", count)
