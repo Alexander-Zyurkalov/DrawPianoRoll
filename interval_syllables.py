44f116a0b018ba74a64c218864eb827eaa2c0d01
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-10-05 13:32:25 +0200

    Add keyboard drawing functionality
    Integrate keyboard visualization by importing matplotlib and defining the `draw_keyboard` function. Additionally, modify interval handling to generate and highlight notes on the visualized keyboard, and update the output to include highlighted notes.

--- interval_syllables.py
+++ interval_syllables.py
@@ -1,4 +1,46 @@
+import matplotlib.pyplot as plt
+
+octaves = 4  # Set the number of octaves you want to see
+ves = 4  # Set the number of octaves you want to see
+keys_per_octave = 12
+white_keys_per_octave = 7
+total_white_keys = octaves * white_keys_per_octave
 bases = ('K', 'T', 'D', 'N', 'M', 'F', 'J', 'G', 'R', 'L', 'P', 'B', 'K')
+note_names = bases
+
+# Define the correct positions of notes on the keyboard
+white_keys = ['K', 'D', 'M', 'F', 'G', 'L', 'B']
+black_keys = ['T', 'N', 'J', 'R', 'P']
+
+# Define white and black key indices for an octave
+white_key_positions = {'K': 0, 'D': 1, 'M': 2, 'F': 3, 'G': 4, 'L': 5, 'B': 6}
+black_key_positions = {'T': 0, 'N': 1, 'J': 3, 'R': 4, 'P': 5}
+def draw_keyboard(file_name: str, highlighted_notes=None):
+   highlighted_notes = highlighted_notes or []
+
+   plt.figure(figsize=(15, 4))
+   plt.xlim(0, total_white_keys)
+   plt.ylim(0, 6)
+
+   # Draw white keys
+   for octave in range(octaves):
+      for i, white_key in enumerate(white_keys):
+         key_index = octave * white_keys_per_octave + i
+         color = 'lightcoral' if f'{white_key}{octave}' in highlighted_notes else 'white'
+         plt.gca().add_patch(plt.Rectangle((key_index, 0), 1, 3, facecolor=color, edgecolor='black'))
+
+   # Draw black keys
+   for octave in range(octaves):
+      for i, black_key in enumerate(black_keys):
+         key_index = octave * white_keys_per_octave + black_key_positions[black_key]
+         color = 'darkred' if f'{black_key}{octave}' in highlighted_notes else 'black'
+         plt.gca().add_patch(plt.Rectangle((key_index + 0.7, 1.5), 0.6, 1.5, facecolor=color, edgecolor='black'))
+
+   plt.axis('off')  # Hide the axes
+   plt.savefig('output/intervals/' + file_name)
+
+
+
 root_suffix = 'u'
 mi_up = 'i'
 ma_up = 'a'
@@ -36,5 +78,7 @@ for i, root in enumerate(bases):
       leftToRight = root + root_suffix + n_note + up_quality
       rightToLeft = n_note + root_suffix + root + do_quality
       img_url = f"piano_{leftToRight}_{rightToLeft}.png"
-      print(f"{leftToRight}\tmnemonic\t{rightToLeft}\t{intervalNames[j]}\t{img_url}")
+      notes = [root+'1', n_note + str((i+j)//12+1)]
+      draw_keyboard(img_url, notes)
+      print(f"{leftToRight}\tmnemonic\t{rightToLeft}\t{intervalNames[j]}\t{img_url}\t{notes}")
 print("count = ", count)
