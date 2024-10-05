013a0a588b94e295f15efedbf76c14034781bd09
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-10-05 18:04:18 +0200

    Optimize keyboard drawing and remove unused imports
    Removed the unnecessary import of `total_keys` and adjusted the black key list. Enhanced the keyboard drawing logic to allow for variable starting points and fixed black key drawing to align correctly with the white keys.

--- interval_syllables.py
+++ interval_syllables.py
@@ -1,44 +1,54 @@
 import matplotlib.pyplot as plt
 
-from draw_piano_roll import total_keys
-
 octaves = 2  # Set the number of octaves you want to see
 keys_per_octave = 12
-white_keys_per_octave = 7
-total_white_keys = octaves * white_keys_per_octave
+total_keys = octaves * keys_per_octave
 bases = ('K', 'T', 'D', 'N', 'M', 'F', 'J', 'G', 'R', 'L', 'P', 'B', 'K')
 note_names = bases
 
 # Define the correct positions of notes on the keyboard
 white_keys = ['K', 'D', 'M', 'F', 'G', 'L', 'B']
-black_keys = ['T', 'N', 'J', 'R', 'P']
+white_keys_per_octave = len(white_keys)
+total_white_keys = octaves * white_keys_per_octave
+# black_keys = ['T', 'N', 'J', 'R', 'P']
+black_keys = ['T', 'N']
+black_keys_per_octave = len(black_keys)
 
 # Define white and black key indices for an octave
 white_key_positions = {'K': 0, 'D': 1, 'M': 2, 'F': 3, 'G': 4, 'L': 5, 'B': 6}
 black_key_positions = {'T': 0, 'N': 1, 'J': 3, 'R': 4, 'P': 5}
+white_key_to_black_key = {'K': 'T', 'D': 'N', 'F': 'J', 'G': 'R', 'L': 'P'}
+
 def draw_keyboard(file_name: str, highlighted_notes=None):
    highlighted_notes = highlighted_notes or []
+   note_indexes = [get_note_index(note) for note in highlighted_notes]
+   start_index = calculate_start_index(note_indexes, total_keys)
+   start_white_key = round(start_index / keys_per_octave * white_keys_per_octave)
 
    plt.figure(figsize=(7, 4))
-   plt.xlim(0, total_white_keys)
+   plt.xlim(-start_white_key, total_white_keys-start_white_key)
    plt.ylim(0, 6)
 
    # Draw white keys
-   for octave in range(octaves):
-      for i, white_key in enumerate(white_keys):
-         key_index = octave * white_keys_per_octave + i
-         color = 'lightcoral' if f'{white_key}{octave}' in highlighted_notes else 'white'
-         plt.gca().add_patch(plt.Rectangle((key_index, 0), 1, 3, facecolor=color, edgecolor='black'))
+   for i in range(-start_white_key, white_keys_per_octave * octaves - start_white_key):
+      white_key = white_keys[i % white_keys_per_octave]
+      octave = i // white_keys_per_octave
+      color = 'lightcoral' if f'{white_key}{octave}' in highlighted_notes else 'white'
+      plt.gca().add_patch(plt.Rectangle((i, 0), 1, 3,
+                                        facecolor=color, edgecolor='black'))
 
-   # Draw black keys
-   for octave in range(octaves):
-      for i, black_key in enumerate(black_keys):
-         key_index = octave * white_keys_per_octave + black_key_positions[black_key]
+   for i in range(-start_white_key, white_keys_per_octave * octaves - start_white_key):
+      white_key = white_keys[i % white_keys_per_octave]
+      octave = i // white_keys_per_octave
+      if white_key in white_key_to_black_key:
+         black_key = white_key_to_black_key[white_key]
          color = 'darkred' if f'{black_key}{octave}' in highlighted_notes else 'black'
-         plt.gca().add_patch(plt.Rectangle((key_index + 0.7, 1.5), 0.6, 1.5, facecolor=color, edgecolor='black'))
+         plt.gca().add_patch(plt.Rectangle((i+0.7, 1.3), (1-0.7)*2, 1.7,
+                                           facecolor=color, edgecolor='black'))
 
    plt.axis('off')  # Hide the axes
    plt.savefig('output/intervals/keyboard/' + file_name)
+   plt.show()
    plt.close()
 
 def get_note_index(note:str):
