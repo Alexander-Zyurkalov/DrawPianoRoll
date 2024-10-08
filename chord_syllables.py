9d6d9d3f1a904a68e6a369461cdbb66dddb68367
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-10-08 19:57:33 +0200

    Refactor chord generation and filtering logic
    Simplify chord note adjustments for inversions and reduce imports. Ensure augmented chords are only generated for root inversion to maintain validity. Removed unused import and adjusted sorting logic for clarity.

--- chord_syllables.py
+++ chord_syllables.py
@@ -1,8 +1,6 @@
-from operator import index
-
-from draw_intervals import draw_piano_roll, draw_keyboard, bases, keys_per_octave
-
 from enum import Enum
+
+from draw_intervals import draw_keyboard, bases, keys_per_octave
 
 
 class ChordType(Enum):
@@ -51,14 +49,17 @@ def make_chord(root_: str, chord_type: ChordType, inversion_type: ChordInversion
    if inversion_type == ChordInversionType.SECOND_INVERSION:
       notes[2] -= 12
       notes = [note+12 for note in notes]
+   if all(note >= 12 for note in notes):
+      notes = [note-12 for note in notes]
 
-   names = [f"{bases[note_i % keys_per_octave]}{note_i // keys_per_octave + base_octave}" for note_i in notes]
-   return sorted(names, key=lambda s: s[1] if len(s) > 1 else '')
+   return [f"{bases[note_i % keys_per_octave]}{note_i // keys_per_octave + base_octave}" for note_i in sorted(notes)]
 
 count = 0
 for i, root in enumerate(bases):
    for inversion_type in ChordInversionType:
       for chord_type in ChordType:
+         if chord_type == ChordType.AUGMENTED and inversion_type != ChordInversionType.ROOT:
+            continue
          chord = make_chord(root, chord_type, inversion_type, 0)
          chord_name = "".join(chord)
          file_name = f"{chord_name}-{chord_type.value}-{inversion_type.value}.png"
