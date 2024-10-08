93887a786f869d322cca5c59ef7a6ba82bdddbe4
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-10-08 19:44:52 +0200

    Fix typos and optimize chord sorting
    Corrected typos in chord inversion calculations using += and -= operators. Improved chord naming by sorting names and updated file naming convention for clarity. Reordered nested loops for consistent logic.

--- chord_syllables.py
+++ chord_syllables.py
@@ -47,21 +47,21 @@ def make_chord(root_: str, chord_type: ChordType, inversion_type: ChordInversion
    if inversion_type == ChordInversionType.ROOT:
       pass
    if inversion_type == ChordInversionType.FIRST_INVERSION:
-      notes[0] =+ 12
+      notes[0] += 12
    if inversion_type == ChordInversionType.SECOND_INVERSION:
-      notes[2] =- 12
+      notes[2] -= 12
+      notes = [note+12 for note in notes]
 
    names = [f"{bases[note_i % keys_per_octave]}{note_i // keys_per_octave + base_octave}" for note_i in notes]
-   # sorted_names = sorted(names, key=lambda s: s[1] if len(s) > 1 else '')
-   return names
+   return sorted(names, key=lambda s: s[1] if len(s) > 1 else '')
 
 count = 0
 for i, root in enumerate(bases):
-   for chord_type in ChordType:
-      for inversion_type in ChordInversionType:
-         chord = make_chord(root, chord_type, inversion_type, 1)
+   for inversion_type in ChordInversionType:
+      for chord_type in ChordType:
+         chord = make_chord(root, chord_type, inversion_type, 0)
          chord_name = "".join(chord)
-         file_name = f"chord_{chord_type}{chord_name}.png"
+         file_name = f"{chord_name}-{chord_type.value}-{inversion_type.value}.png"
          print(chord_name)
          draw_keyboard(file_name, 'output/chords/keyboard/', chord)
          count += 1
