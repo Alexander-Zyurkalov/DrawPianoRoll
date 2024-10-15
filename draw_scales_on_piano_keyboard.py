5f7ba9bd0d8d73e70da760aba92ae0561c7b61c5
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2024-10-15 19:39:52 +0200

    Add directional support for generating scale modes
    Introduced a Direction enum to specify the direction of scale generation (UP or DOWN). Modified the make_modes function to handle scale intervals based on the specified direction. Updated the code to render keyboard images for both upward and downward scales.

--- draw_scales_on_piano_keyboard.py
+++ draw_scales_on_piano_keyboard.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
@@ -62,21 +63,29 @@ class ModeNotes:
    notes: list[str]
    colours: dict[str, str]
 
-# Function to generate colors for the scale intervals
-def make_modes(root: str, scale_type: str, base_octave: int = 1) -> ModeNotes:
+class Direction(Enum):
+   UP = 1
+   DOWN = 2
 
+# Function to generate colors for the scale intervals
+def make_modes(root: str, scale_type: str, base_octave: int = 1, direction: Direction = Direction.UP) -> ModeNotes:
    root_index = note_names.index(root)
 
+   scale_intervals = []
    # Calculate the notes in the scale
-   scale_intervals = scales_intervals[scale_type]
+   if direction == Direction.UP:
+      scale_intervals = scales_intervals[scale_type][len(scales_intervals[scale_type]) // 2:]
+   if direction == Direction.DOWN:
+      scale_intervals = scales_intervals[scale_type][:len(scales_intervals[scale_type]) // 2 + 1]
+
    note_colours: Dict[str, str] = {}
+
    # Assign colours for notes in the scale
    for i, interval in enumerate(scale_intervals):
       note_index = (root_index + interval_semitones[interval]) % keys_per_octave
       note_name = note_names[note_index]
       octave_adjustment = (root_index + interval_semitones[interval]) // keys_per_octave
       full_note_name = f'{note_name}{base_octave + octave_adjustment}'
-      print(full_note_name)
       color = get_interval_color(interval)
       note_colours[full_note_name] = color
 
@@ -84,6 +93,7 @@ def make_modes(root: str, scale_type: str, base_octave: int = 1) -> ModeNotes:
       [note for note in note_colours.keys()],
       note_colours
    )
+
 
 note = 'B'
 scale = 'Ionian'
@@ -95,6 +105,9 @@ scale = 'Ionian'
 # highlighted_notes = make_colours(note, 'Aeolian', base_octave=1)
 # highlighted_notes = make_colours(note, 'Locrian', base_octave=1)
 
-mode = make_modes(note, scale, base_octave=1)
-file_name = f"scales-{scale}-{note}.png"
-draw_keyboard("output/scales/", file_name, mode.notes, mode.colours, 3)
+mode_up = make_modes(note, scale, base_octave=1, direction=Direction.UP)
+mode_do = make_modes(note, scale, base_octave=1, direction=Direction.DOWN)
+file_name_up = f"scale-{scale}-{note}-up.png"
+file_name_do = f"scale-{scale}-{note}-do.png"
+draw_keyboard("output/scales/", file_name_up, mode_up.notes, mode_up.colours, 2)
+draw_keyboard("output/scales/", file_name_do, mode_do.notes, mode_do.colours, 2,)
